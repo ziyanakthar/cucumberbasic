6e27230afbd2ac620b9f053c109b184aa340e394
Ziyan <ziyanakthar@aol.com>
2018-12-10 18:01:47 +0000

    chrome

--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -23,7 +23,7 @@ public class Hook extends BaseUtil{
 
         System.out.println("Opening the browser : MOCK");
 
-        System.setProperty("webdriver.chrome.driver", "/root/chromedriver"
+        System.setProperty("webdriver.chrome.driver", "/root/chromedriver");
         base.Driver = new ChromeDriver();
 
         //Passing a dummy WebDriver instance step info
